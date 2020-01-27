f4d16cd6255360bce498bc26e8efcf42cb973adb
Twinparadox <nww731@gmail.com>
2020-01-27 22:31:50 +0900

    Update RF, SVR
    RandomForest
    SupportVectorRegression
    with SlidingWindows

--- HousePricePrediction/SVR.py
+++ HousePricePrediction/SVR.py
@@ -2,15 +2,14 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-scaler = StandardScaler()
-
+scaler = MinMaxScaler(feature_range=(0, 1))
 
 def splitTrainTest(data, train_period, test_period, multiVariate=False, gtruthName='SPI'):
     if not multiVariate:
@@ -29,19 +28,19 @@ def splitTrainTest(data, train_period, test_period, multiVariate=False, gtruthNa
 def makeSlidingWindows(origin_train, origin_test, train, test, lookBack=2, multiVariate=False, gtruthName='SPI'):
     train_df = pd.DataFrame(train, index=origin_train.index)
     test_df = pd.DataFrame(test, index=origin_test.index)
-
+        
     train_df.columns = origin_train.columns
     test_df.columns = origin_test.columns
-
+    
     if not multiVariate:
         column_list = list(train_df)
         for s in range(1, lookBack + 1):
             tmp_train = train_df[column_list].shift(s)
             tmp_test = test_df[column_list].shift(s)
-
+    
             tmp_train.columns = "shift_" + tmp_train.columns + "_" + str(s)
             tmp_test.columns = "shift_" + tmp_test.columns + "_" + str(s)
-
+    
             train_df[tmp_train.columns] = train_df[column_list].shift(s)
             test_df[tmp_test.columns] = test_df[column_list].shift(s)
 
@@ -49,19 +48,19 @@ def makeSlidingWindows(origin_train, origin_test, train, test, lookBack=2, multi
 
     else:
         column_list = list(train_df)
-
+    
         for s in range(1, lookBack + 1):
             tmp_train = train_df[column_list].shift(s)
             tmp_test = test_df[column_list].shift(s)
-
+    
             tmp_train.columns = "shift_" + tmp_train.columns + "_" + str(s)
             tmp_test.columns = "shift_" + tmp_test.columns + "_" + str(s)
-
+    
             train_df[tmp_train.columns] = train_df[column_list].shift(s)
             test_df[tmp_test.columns] = test_df[column_list].shift(s)
 
         return train_df, test_df
-
+        
 
 def plot():
     pass
@@ -94,15 +93,13 @@ if __name__ == "__main__":
     X_test1 = test1_df.dropna().drop('SPI', axis=1).values
     Y_test1 = test1_df.dropna()[['SPI']].values
 
-    pipe_model = Pipeline([('scl', MinMaxScaler(feature_range=(0, 1))), ('clf', RandomForestRegressor())])
-    n_estimators = [100,200,300,400,500]
-    max_features = ['auto','sqrt','log2']
-    max_depths = [3,4,5,6,7]
-    criterions = ['mse', 'mae']
-    param_grid = [{'clf__n_estimators': n_estimators,
-                   'clf__max_depth': max_depths,
-                   'clf__max_features': max_features,
-                   'clf__criterion': criterions}]
+    pipe_model = Pipeline([('scl', MinMaxScaler(feature_range=(0, 1))), ('clf', SVR())])
+    param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]
+    param_range2 = [0.0001, 0.001, 0.01, 0.1]
+    param_grid = [{'clf__C': param_range,
+                   'clf__kernel': ['rbf'],
+                   'clf__gamma': param_range,
+                   'clf__epsilon': param_range2}]
     gs = GridSearchCV(estimator=pipe_model, param_grid=param_grid,
                       scoring='neg_mean_absolute_error', cv=5, iid=True, n_jobs=-1)
     gs = gs.fit(X_train1, Y_train1)
@@ -113,9 +110,9 @@ if __name__ == "__main__":
     best_model = gs.best_estimator_
 
     Y_pred = best_model.predict(X_test1)
-
-    Y_test1 = scaler.inverse_transform(Y_test1.reshape(-1, 1))
-    Y_pred = scaler.inverse_transform(Y_pred.reshape(-1, 1))
+    
+    Y_test1 = scaler.inverse_transform(Y_test1.reshape(-1,1))
+    Y_pred = scaler.inverse_transform(Y_pred.reshape(-1,1))
 
     MAE = mean_absolute_error(Y_test1, Y_pred)
     print("test MAE", MAE)
